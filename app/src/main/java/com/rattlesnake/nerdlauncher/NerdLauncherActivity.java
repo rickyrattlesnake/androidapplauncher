a4f4dc3f0bf8c3360a1d2245971bb951b695dcac
rattlesnake <ricky.ratnayake@gmail.com>
2017-06-22 23:14:44 +1000

    first commit

---
+++ app/src/main/java/com/rattlesnake/nerdlauncher/NerdLauncherActivity.java
@@ -0,0 +1,18 @@
+package com.rattlesnake.nerdlauncher;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+
+public class NerdLauncherActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return NerdLauncherFragment.newInstance();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nerd_launcher);
+    }
+}
---
+++ app/src/main/java/com/rattlesnake/nerdlauncher/NerdLauncherFragment.java
@@ -0,0 +1,29 @@
+package com.rattlesnake.nerdlauncher;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NerdLauncherFragment extends Fragment {
+    private RecyclerView mRecyclerView;
+
+    static Fragment newInstance() {
+        return new NerdLauncherFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_nerd_launcher, container, false);
+        mRecyclerView = (RecyclerView)
+            v.findViewById(R.id.fragment_nerd_launcher_recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return v;
+    }
+}
